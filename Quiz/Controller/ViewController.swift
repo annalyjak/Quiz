88f5c5f166547f14b2436076a908ea58ca1321ed
Anna (Mac) <anna@annas-mac2.local>
2018-05-28 06:28:54 -0700

    images

--- Quiz/Controller/ViewController.swift
+++ Quiz/Controller/ViewController.swift
@@ -9,15 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var informations: [Information] = [Information(), Information(), Information()]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationController?.navigationBar.prefersLargeTitles = false
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showInformation" {
+            let destinationController = segue.destination as! InformationViewController
+            destinationController.information = informations[1]
+        }
     }
 
 
